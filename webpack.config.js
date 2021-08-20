8d97426ec091f4e8ed18ba8fe283d8c33e336d88
Sarah Bakir <sarahbakir.k@gmail.com>
2021-08-21 00:43:33 +0200

    phase 4 - webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
     entry: './src/client/index.js',
@@ -12,5 +13,11 @@ module.exports = {
                 loader: "babel-loader"
             }
         ]
-    }
+    }, 
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 }
